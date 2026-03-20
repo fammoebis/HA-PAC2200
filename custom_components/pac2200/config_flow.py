3cef68a0242896a3dbafc567301b99023a0aea57
fmoebis <fmoebis@gmail.com>
2026-03-20 11:28:05 +0000

    fix

--- custom_components/pac2200/config_flow.py
+++ custom_components/pac2200/config_flow.py
@@ -1,12 +1,18 @@
 import voluptuous as vol
 from homeassistant import config_entries
+
 from .const import DOMAIN, DEFAULT_PORT, DEFAULT_SLAVE
 
+
 class Pac2200ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
 
     async def async_step_user(self, user_input=None):
         if user_input is not None:
-            return self.async_create_entry(title=user_input["host"], data=user_input)
+            return self.async_create_entry(
+                title=user_input["host"],
+                data=user_input
+            )
 
         return self.async_show_form(
             step_id="user",
