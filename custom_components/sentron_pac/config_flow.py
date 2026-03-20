dacd91e5a98fcd06c9e24b8dbc54da3eb21fa839
fmoebis <fmoebis@gmail.com>
2026-03-20 17:37:41 +0000

    FIX

--- custom_components/sentron_pac/config_flow.py
+++ custom_components/sentron_pac/config_flow.py
@@ -1,5 +1,6 @@
 import voluptuous as vol
 from homeassistant import config_entries
+import homeassistant.helpers.config_validation as cv
 from .const import DOMAIN, DEFAULT_PORT, DEFAULT_SLAVE
 
 class SentronConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -7,17 +8,21 @@ class SentronConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
+        errors = {}
         if user_input is not None:
+            # Erstellt den Eintrag in der UI
             return self.async_create_entry(
                 title=f"PAC2200 ({user_input['host']})", 
                 data=user_input
             )
 
+        # Das Formular, das dem Nutzer angezeigt wird
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
                 vol.Required("host"): str,
                 vol.Required("port", default=DEFAULT_PORT): int,
                 vol.Required("slave", default=DEFAULT_SLAVE): int,
-            })
+            }),
+            errors=errors,
         )
--- custom_components/sentron_pac/const.py
+++ custom_components/sentron_pac/const.py
@@ -1,3 +1,4 @@
+# custom_components/sentron_pac/const.py
 DOMAIN = "sentron_pac"
 DEFAULT_PORT = 502
 DEFAULT_SLAVE = 1
